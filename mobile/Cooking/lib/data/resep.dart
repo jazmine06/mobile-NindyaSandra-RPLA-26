13be43750c074c63f105aa0e7c44e0db3bb4202c
jazmine06 <jasminezulcha@gmail.com>
2022-12-07 21:26:19 +0700

    first commit

---
+++ mobile/Cooking/lib/data/resep.dart
@@ -0,0 +1,37 @@
+class resep {
+  String name, htm, tutorial, image;
+
+  resep(
+      {required this.name,
+      required this.htm,
+      required this.tutorial,
+      required this.image});
+}
+
+List<resep> dataResep = [
+  resep(
+      name: 'Timnas Indonesia U-20 akan Hadapi Jepang pada 7 November di Spanyol',
+      htm: 'BOLA',
+      tutorial:
+          'TOKYO - Timnas Indonesia U-20 dipastikan menghadapi Timnas Jepang U-20 pada laga uji coba di Spanyol. Pertandingan tersebut dilangsungkan dalam rangka pemusatan latihan (TC) Timnas Indonesia U-20 di Negeri Matador.Seperti diketahui, tim asuhan Shin Tae-yong menjalani pemusatan latihan di Turki sejak 10 Oktober silam. TC Timnas Indonesia U-20 di Turki dijadwalkan berakhir pada Sabtu (5/11/2022), dan rombongan langsung bertolak ke Spanyol. Agenda Timnas Indonesia U-20 ternyata bertepatan dengan jadwal Timnas Jepang U-20. Tim Samurai Biru muda diagendakan menjalani TC di sana pada 3-13 November 2022.Dilansir dari laman Yahoo News JP, Sabtu (5/11/2022), Timnas Jepang U-20 juga akan menjalani uji coba. Tiga lawan yang akan dihadapi adalah Timnas Indonesia U-20, Timnas Belgia U-20, dan Algeciras FC.',
+      image: 'assets/nestumcookies.jpeg'),
+  resep(
+      name: 'Jorge Martin Siap Bertarung dengan Fabio Quartaro di MotoGP Valencia 2022',
+      htm: 'BALAP',
+      tutorial:
+          'VALENCIA – Pembalap Pramac Ducati, Jorge Martin mengincar kemenangan di MotoGP Valencia 2022 setelah menyabet pole position di sesi kualifikasi. Dia pun mengatakan siap bertarung dengan Fabio Quartararo. Martin tampil luar biasa dalam sesi kualifikasi di Sirkuit Ricardo Tormo yang berlangsung pada Sabtu (5/11/2022). Dia sukses meraih tempat terdepan di garis start dengan mencatatkan waktu tercepat di angka 1 menit 29,621 detik.',
+      image: 'assets/waferkeju.png'),
+  resep(
+      name: 'Livoli Divisi Utama 2022 - Petrokimia Gresik Juara Babak Reguler Putaran Pertama',
+      htm: 'VOLI',
+      tutorial:
+          'Petrokimia Pupuk Indonesia Gresik berhasil menjuarai babak reguler putaran pertama, Livoli Divisi Utama 2022. Petrokimia Gresik keluar sebagai juara setelah di final menundukkan juara bertahan kemenangan diraih tim asuhan Ayub Hidayat ini, termasuk Bank Jatim. Mereka tampil apik untuk menang atas Bank Jatim Surabaya 3-1 (25-14, 15-25, 25-20, 25-17), di GOR Tawangalun Banyuwangi, Sabtu (15/10/2022). Dengan kemenangan tersebut, Petrokimia belum terkalahkan di seri Banyuwangi ini',
+      image: 'assets/kuegarpu.png'),
+  resep(
+      name: 'PUBG Berasal dari Negara Mana? Yuk Ketahui Jawabannya!',
+      htm: 'HOBI',
+      tutorial:
+          'PUBG berasal dari negara mana? menjadi pertanyaan yang sering dilontarkan oleh para pecinta Esports. Game PUBG merupakan salah satu gim bergenre Battle Royal yang kini tengah populer di dunia.',
+      image: 'assets/putrisalju.png'),
+  
+];
